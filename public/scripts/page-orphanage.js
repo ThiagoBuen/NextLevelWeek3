001c314aafa72148869ae4d1c7e0f095bc6c6ff2
ThiagoBuen <thiagobueno@gea.inatel.br>
2020-10-15 22:14:16 -0300

    15/10 - Page orphanage

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,40 @@
+const options = {
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false,
+};
+//create map
+
+const map = L.map('mapid', options).setView([-22.2635347, -45.6965906], 15);
+
+//create and add tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon = L.icon({
+  iconUrl: './public/images/map-marker.svg',
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+L.marker([-22.2635347, -45.6965906], { icon: icon }).addTo(map);
+
+function selectImage(event) {
+  const button = event.currentTarget;
+
+  const buttons = document.querySelectorAll('.images button');
+
+  buttons.forEach((currentButton) => {
+    currentButton.classList.remove('active');
+  });
+
+  const image = button.children[0];
+  const imageContainer = document.querySelector('.orphanage-details > img');
+
+  imageContainer.src = image.src;
+
+  button.classList.add('active');
+}
--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,12 +1,30 @@
-const map = L.map('mapid').setView([51.505, -0.09], 13);
+//create map
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
-/*
-L.marker([51.5, -0.09])
+const map = L.map('mapid').setView([-22.2635347, -45.6965906], 15);
+
+//create and add tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon = L.icon({
+  iconUrl: './public/images/map-marker.svg',
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+//create popup overlay
+
+const popup = L.popup({
+  closeButton: false,
+  className: 'map-popup',
+  minWidth: 240,
+  minHeight: 240,
+}).setContent(
+  'Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>'
+);
+//"
+
+L.marker([-22.2635347, -45.6965906], { icon: icon })
   .addTo(map)
-  .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-  .openPopup();
-*/
+  .bindPopup(popup);
